e58e5ff091361f5d91eec6237560cd933e75de47
SH <SH@DESKTOP-PVBF9IT>
2021-10-05 12:55:59 +0900

    usermanager>join,login,logout,remove

--- Lv9Shhop_SH/src/Shop.java
+++ Lv9Shhop_SH/src/Shop.java
@@ -4,9 +4,83 @@ public class Shop {
 	Scanner scan = new Scanner(System.in);
 	ItemManager im = new ItemManager();
 	UserManager um = new UserManager();
-	
+
+	// 기본화면
+	public boolean mainMenu() {
+		boolean run = true;
+		while (run) {
+			System.out.println("1.가입 2.탈퇴 3.로그인 4.로그아웃 / 100.관리자 0.종료");
+			System.out.print("메뉴선택 : ");
+			String input = scan.next();
+
+			try {
+				int sel = Integer.parseInt(input);
+				if (sel == 1) {
+					um.joinMember();
+				} else if (sel == 2) {
+					um.removeMember();
+				} else if (sel == 3) {
+					if (um.login()) {
+						loginMenu();
+					}
+				} else if (sel == 4) {
+					um.logout();
+				} else if (sel == 100) {
+					managerMenu();
+				} else if (sel == 0) {
+					run = false;
+				}
+			} catch (Exception e) {
+			}
+		}
+		return run;
+	}
+
+	// 로그인시
+	private void loginMenu() {
+		boolean run = true;
+		while (true) {
+			System.out.println("1.쇼핑 2.장바구니목록 3.뒤로가기");
+			String input = scan.next();
+			try {
+				int sel = Integer.parseInt(input);
+				if (sel == 1) {
+					shopMenu();
+				} else if (sel == 2) {
+					cartMenu();
+				} else if (sel == 3) {
+					break;
+				}
+			} catch (Exception e) {
+			}
+
+		}
+
+	}
+
+	// 로그인-쇼핑
+	private void shopMenu() {
+		// TODO Auto-generated method stub
+
+	}
+
+	// 로그인-장바구니목록
+	private void cartMenu() {
+		// TODO Auto-generated method stub
+
+	}
+
+	// 관리자 메뉴
+	private void managerMenu() {
+		// TODO Auto-generated method stub
+
+	}
+
 	public void run() {
-		
+		boolean isRun = true;
+		while (isRun) {
+			isRun = mainMenu();
+		}
 	}
 
 }
--- Lv9Shhop_SH/src/UserManager.java
+++ Lv9Shhop_SH/src/UserManager.java
@@ -1,4 +1,93 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Vector;
 
 public class UserManager {
+	Random ran = new Random();
+	Scanner scan = new Scanner(System.in);
+	Vector<User> userList = new Vector<>(); // 전체 유저 리스트
+	int userLog = -1; // 로그인기본값 -1
 
+	public UserManager() {
+//		init();
+	}
+
+	public void init() {
+		String[] a = { "김", "박", "이", "최", "정", "오" };
+		String[] b = { "철", "병", "만", "여", "아", "영" };
+		String[] c = { "수", "욱", "수", "정", "름", "희" };
+		for (int i = 0; i < 10; i++) {
+			int r = ran.nextInt(a.length);
+			String name = a[r];
+			r = ran.nextInt(b.length);
+			name += b[r];
+			r = ran.nextInt(c.length);
+			name += c[r];
+			User temp = new User(name, ran.nextInt(5000));
+			userList.add(temp);
+		}
+	}
+
+	
+
+	public boolean login() {
+		userLog = -1;
+		System.out.print("ID:");
+		String id = scan.next();
+		for (int i = 0; i < userList.size(); i++) {
+			if (id.equals(userList.get(i).id)) {
+				userLog = i;
+				break;
+			}
+		}
+		if (userLog != -1) {
+			System.out.println("로그인 성공!");
+			System.out.println(userList.get(userLog).id + "님 환영합니다!");
+		} else {
+			System.out.println("존재하지 않는 아이디입니다.");
+		}
+		return false;
+	}
+
+	public void logout() {
+		if (userLog == -1) {
+			System.out.println("이미 로그아웃 상태입니다.");
+		} else {
+			System.out.println("로그아웃 완료");
+		}
+		userLog = -1;
+	}
+
+	public void joinMember() {
+		System.out.print("ID:");
+		String id = scan.next();
+		boolean check=true;
+		for (int i = 0; i < userList.size(); i++) {
+			if (userList.get(i).id.equals(id)) {
+				check=false;
+			}			
+		}
+		if(check) {
+			User temp = new User(id, 0);
+			userList.add(temp);
+			System.out.println(temp.id + "님 가입완료되었습니다.");			
+		}else {
+			System.out.println("이미 존재하는 아이디입니다.");
+		}
+	}
+
+	public void removeMember() {
+		System.out.print("ID:");
+		String id = scan.next();
+		for (int i = 0; i < userList.size(); i++) {
+			if (userList.get(i).id.equals(id)) {
+				userList.remove(i);
+			}
+		}
+	}
+	public void printUser() {
+		for (int i = 0; i < userList.size(); i++) {
+			userList.get(i).print();
+		}
+	}
 }
