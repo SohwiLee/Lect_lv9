6c0ac409755c224fdc64d49d62cd0d240cbb79ff
SH <SH@DESKTOP-FOD9VD0>
2021-10-19 11:30:34 +0900

    myPoly Start

---
+++ myPoly/src/Models/Unit.java
@@ -0,0 +1,46 @@
+package Models;
+
+public abstract class Unit {
+	int curhp;
+	int maxhp;
+	int power;
+	String name;
+	String state = "보통";
+
+	Unit() {
+	}
+
+	Unit(String name, int max, int pow) {
+		this.name = name;
+		this.maxhp = max;
+		this.curhp = max;
+		this.power = pow;
+	}
+
+	void init(int max, int pow) {
+		this.maxhp = max;
+		this.curhp = max;
+		this.power = pow;
+	}
+
+	void init(String name, int max, int pow) {
+		this.name = name;
+		this.maxhp = max;
+		this.curhp = max;
+		this.power = pow;
+	}
+
+	void attack(Unit target) { // 기본공격
+		target.curhp -= power;
+		System.out.println("[" + this.name + "]" + target.name + "에게 " + this.power + "의 데미지를 입혔다!");
+		if (target.curhp <= 0) {
+			target.curhp = 0;
+			System.out.println(target.name + "이 쓰러졌습니다!");
+		}
+	}
+
+	void printData() {
+		System.out.println("[" + this.name + "] " + this.curhp + "/" + this.maxhp + " pow:" + this.power);
+	}
+
+}
