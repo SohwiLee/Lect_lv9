d3c9a376765b7b7c82afbbaff71d1a89ace3d185
SH <SH@DESKTOP-PVBF9IT>
2021-10-20 09:16:48 +0900

    lv10 test commit

--- myPoly/src/Controller/GameManager.java
+++ myPoly/src/Controller/GameManager.java
@@ -1,5 +1,48 @@
 package Controller;
 
-public class GameManager {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
 
+import Models.Stage;
+
+public class GameManager {
+	Random ran = new Random();
+	static Scanner scan = new Scanner(System.in);
+	public static String nextStage;
+	public static String curStage;
+	Map<String, Stage> stageList = new HashMap<String, Stage>();
+
+	GameManager() {
+		stageList.put("Title", new StageTitle());
+		stageList.put("Battle", new StageBattle());
+		stageList.put("Lobby", new StageLobby());
+		nextStage = "Title";
+	}
+
+	public boolean changeStage() {
+		System.out.println("CUR:" + curStage + "/NEXT:" + nextStage);
+		if (curStage.equals(nextStage)) {
+			return true;
+		}
+		curStage = nextStage;
+
+		Stage stage = stageList.get(curStage);
+//		stage.init();
+		boolean run = true;
+		while (true) {
+			run = stage.update();
+			if (!run) {
+				break;
+			}
+		}
+
+		if (nextStage.equals("")) {
+			return false;
+		} else {
+			return false;
+		}
+
+	}
 }
